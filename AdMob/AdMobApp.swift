3fd47524aa79a74c0fe338f729ce1d98df3a6e39
Samy Abderraouf Mehdid <sr.mehdid@gmail.com>
2023-11-14 10:33:59 +0100

    added proj

---
+++ AdMob/AdMobApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdMobApp.swift
+//  AdMob
+//
+//  Created by Samy Mehdid on 14/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AdMobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
